8de33c299e52b3f0c6ed2aaac72601129e675c0e
JAE HOON LEE <jazzyrain722@naver.com>
2023-03-24 18:02:18 +0900

    무한스크롤링 적용

--- src/Pages/Search/index.js
+++ src/Pages/Search/index.js
@@ -1,3 +1,6 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import MovieApi from 'Apis/movieApi';
+import { useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import SearchedMovies from './components/searchedMovies';
@@ -5,12 +8,59 @@ import SearchedMovies from './components/searchedMovies';
 function SearchPage() {
 	const location = useLocation();
 	const searchParams = new URLSearchParams(location.search);
+
 	const word = searchParams.get('word');
+
+	const {
+		data: searchResults,
+		isFetchingNextPage,
+		fetchNextPage,
+		hasNextPage,
+	} = useInfiniteQuery(
+		['searchResults', word],
+		({ pageParam = 1 }) => MovieApi.getSearch(word, pageParam),
+		{
+			onError: () => console.log('에러'),
+			getNextPageParam: (lastPage, allPages) => {
+				if (lastPage.data.page < lastPage.data.total_pages) {
+					return lastPage.data.page + 1;
+				} else {
+					return undefined;
+				}
+			},
+		},
+	);
+	const handleScroll = useCallback(
+		node => {
+			if (node && !isFetchingNextPage && hasNextPage) {
+				const observer = new IntersectionObserver(
+					entries => {
+						if (entries[0].isIntersecting) {
+							fetchNextPage();
+						}
+					},
+					{
+						rootMargin: '0px 0px 500px 0px',
+					},
+				);
+				observer.observe(node);
+			}
+		},
+		[hasNextPage, fetchNextPage],
+	);
 
 	return (
 		<Wrapper>
 			<h1>Search "{word}"</h1>
-			<SearchedMovies />
+			{searchResults &&
+				searchResults.pages.map(
+					page =>
+						page &&
+						page.data.results.map(movie => (
+							<SearchedMovies key={movie.id} movie={movie} />
+						)),
+				)}
+			<div ref={handleScroll}></div>
 		</Wrapper>
 	);
 }
@@ -21,3 +71,4 @@ const Wrapper = styled.div`
 	border: 2px solid cornflowerblue;
 	margin: 0 15%;
 `;
+
